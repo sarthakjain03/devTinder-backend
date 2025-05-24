6615e457e75ec523a2a5b7d2fe8095ca3a505cbb
Sarthak Jain <jainsarthak2003@gmail.com>
2025-05-24 16:12:49 +0530

    added get, delete and patch user apis

--- src/app.js
+++ src/app.js
@@ -17,7 +17,68 @@ app.post("/signup", async (req, res) => {
         res.send("User created successfully");
         
     } catch (error) {
-        res.status(400).send("Error occurred while signing up: " + error?.message);
+        res.status(500).send("Error occurred while signing up: " + error?.message);
+    }
+})
+
+app.get("/user", async (req, res) => {
+    try {
+        const userEmail = req.body?.email;
+        const user = await User.findOne({ email: userEmail });
+        if (!user) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(user);
+        }
+        
+    } catch (error) {
+        res.status(500).send("Error occurred while fetching user: " + error?.message);
+    }
+})
+
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        if (users?.length === 0) {
+            res.status(404).send("No User Found");
+        } else {
+            res.send(users);
+        }
+        
+    } catch (error) {
+        res.status(500).send("Error occurred while fetching users: " + error?.message);
+    }
+})
+
+app.delete("/user", async (req, res) => {
+    try {
+        const userId = req.body?.userId;
+        const user = await User.findByIdAndDelete(userId);
+        if (!user) {
+            res.status(404).send("User not found");
+        } else {
+            res.send("User deleted successfully");
+        }
+        
+    } catch (error) {
+        res.status(500).send("Error occurred while deleting user: " + error?.message);
+    }
+})
+
+app.patch("/user", async (req, res) => {
+    try {
+        const userId = req.body?.userId;
+        const data = req.body
+        const user = await User.findByIdAndUpdate(userId, data);
+        // Same as const user = await User.findByIdAndUpdate({ _id: userId }, data);
+        if (!user) {
+            res.status(404).send("User not found");
+        } else {
+            res.send("User updated successfully");
+        }
+        
+    } catch (error) {
+        res.status(500).send("Error occurred while updating user: " + error?.message);
     }
 })
 
