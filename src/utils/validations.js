ac9d1dbc6bd81db8bf7bc0d8f58d2e41990c4dad
Sarthak Jain <jainsarthak2003@gmail.com>
2025-07-24 23:08:59 +0530

    updated validations

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,14 +1,14 @@
 const validator = require("validator");
 
 const validateLoginData = (req) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-        throw new Error("All fields are required");
-    }
-    if (!validator.isEmail(email)) {
-        throw new Error("Invalid email format");
-    }
-}
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new Error("All fields are required");
+  }
+  if (!validator.isEmail(email)) {
+    throw new Error("Invalid email format");
+  }
+};
 
 const validateSignUpData = (req) => {
   const { name, email, password, age } = req.body;
@@ -30,29 +30,51 @@ const validateSignUpData = (req) => {
 };
 
 const validateProfileEditData = (req) => {
-  const allowedEditFields = ["age", "gender", "photoUrl", "about", "skills"];
+  const allowedEditFields = [
+    "age",
+    "gender",
+    "photoUrl",
+    "about",
+    "skills",
+    "name",
+  ];
 
-  const isEditAllowed = Object.keys(req.body)?.every((field) => allowedEditFields.includes(field));
+  const isEditAllowed = Object.keys(req.body)?.every((field) =>
+    allowedEditFields.includes(field)
+  );
   if (!isEditAllowed) {
     throw new Error("Invalid Edit Request");
   }
 
-  const { age, gender, photoUrl, about, skills } = req.body;
-  if (age && age < 18) {
+  const { age, gender, photoUrl, about, skills, name } = req.body;
+  if (!name || name.length < 3 || name.length > 50) {
+    throw new Error("Name must be between 3 and 50 characters");
+  }
+  if (!age || age < 18) {
     throw new Error("Age must be at least 18");
   }
-  if (gender && gender !== "Male" && gender !== "Female" && gender !== "Other") {
+  if (
+    !gender ||
+    (gender !== "Male" && gender !== "Female" && gender !== "Other")
+  ) {
     throw new Error("Gender must be either Male, Female or Other");
   }
   if (photoUrl && !validator.isURL(photoUrl)) {
     throw new Error("Photo Url must be a valid URL");
   }
-  if (about && about?.length > 200) {
+  if (!about || about?.length > 200) {
     throw new Error("About must be less than 200 characters");
+  }
+  if (skills && !Array.isArray(skills)) {
+    throw new Error("Skills must be an array");
   }
   if (skills && skills?.length > 20) {
     throw new Error("Skills must be less than 20");
   }
-}
+};
 
-module.exports = { validateSignUpData, validateLoginData, validateProfileEditData };
+module.exports = {
+  validateSignUpData,
+  validateLoginData,
+  validateProfileEditData,
+};
